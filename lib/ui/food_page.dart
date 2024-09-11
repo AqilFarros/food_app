0296a77cd7fe0cd9622afe55f3d414ad7333c85c
AqilFarros <aqilghonim@gmail.com>
2024-09-11 21:13:44 +0700

    update

--- lib/ui/food_page.dart
+++ lib/ui/food_page.dart
@@ -24,8 +24,75 @@ class _FoodPageState extends State<FoodPage> {
           color: darkColor,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Food Market.",
+                    style: heading1,
+                  ),
+                  Text(
+                    "Let's obtain some food.",
+                    style: heading2,
+                  ),
+                ],
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://i.pinimg.com/736x/c8/4b/1b/c84b1bc7fb9fe438e9ac111af9db1b94.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
+        Container(
+          height: 220,
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(
+            vertical: defaultMargin,
+          ),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: mockFoods
+                .map(
+                  (food) => Padding(
+                    padding: EdgeInsets.only(
+                        left: food == mockFoods.first ? defaultMargin : 0,
+                        right: defaultMargin),
+                    child: FoodCard(
+                      food: food,
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          color: Colors.black,
+          child: Column(
+            children: [
+              Tabbar(
+                selectedIndex: selectedIndex,
+                titles: ["New Taste", "Popular", "Recommended"],
+                onTap: (index) {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
--- lib/ui/widget/widgets.dart
+++ lib/ui/widget/widgets.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
+import 'package:food_app/model/models.dart';
 import 'package:food_app/shared/shared.dart';
 import 'package:get/get.dart';
+import 'package:supercharged/supercharged.dart';
 
 part 'bottom_navbar.dart';
+part 'food_card.dart';
+part 'rating_star.dart';
+part 'tabbar.dart';
