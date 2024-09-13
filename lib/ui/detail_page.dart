29228477c14a9006de1ebb0774660ac2e49af4f4
AqilFarros <aqilghonim@gmail.com>
2024-09-13 11:33:46 +0700

    update

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -15,6 +15,8 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int quantity = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,19 +46,18 @@ class _DetailPageState extends State<DetailPage> {
           SafeArea(
             child: ListView(
               children: [
-                GestureDetector(
-                  onTap: () {
-                    if (widget.onBackButtonPressed != null) {
-                      widget.onBackButtonPressed!();
-                    }
-                  },
-                  child: Container(
-                    height: 100,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: defaultMargin,
-                    ),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
+                Container(
+                  height: 100,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: defaultMargin,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        if (widget.onBackButtonPressed != null)
+                          widget.onBackButtonPressed!();
+                      },
                       child: Container(
                         padding: const EdgeInsets.all(3),
                         width: 30,
@@ -69,7 +70,97 @@ class _DetailPageState extends State<DetailPage> {
                       ),
                     ),
                   ),
-                )
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 180),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 26,
+                    horizontal: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${widget.food.name}",
+                                  style: heading2,
+                                  maxLines: 1,
+                                ),
+                                const SizedBox(
+                                  width: 24,
+                                ),
+                                RatingStar(
+                                  rate: widget.food.rate,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    quantity = max(1, quantity - 1);
+                                  });
+                                },
+                                child: Container(
+                                  width: 35,
+                                  height: 35,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/btn_min.png'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 50,
+                                child: Text(
+                                  quantity.toString(),
+                                  textAlign: TextAlign.center,
+                                  style: heading3.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    color: darkColor,
+                                  ),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    quantity = min(999, quantity + 1);
+                                  });
+                                },
+                                child: Container(
+                                  width: 35,
+                                  height: 35,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/btn_add.png'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
