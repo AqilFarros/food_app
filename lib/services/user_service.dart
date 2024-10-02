bbbdcba296da831cfdceb93fa953e28df1bc550b
AqilFarros <aqilghonim@gmail.com>
2024-10-02 10:43:28 +0700

    update

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -36,4 +36,77 @@ class UserService {
     // // login gagal
     // return ApiReturnValue(message: "Email atau password salah");
   }
+
+  static Future<ApiReturnValue<User>> signUp(User user, String password,
+      {File? pictureFile, http.Client? client}) async {
+    if (client == null) {
+      client = http.Client();
+    }
+
+    String url = baseUrl + "/register";
+
+    var response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: {
+        'name': user.name,
+        'email': user.email,
+        'password': password,
+        'password_confirmation': password,
+        'address': user.address,
+        'city': user.city,
+        'houseNumber': user.houseNumber,
+        'phoneNumber': user.phoneNumber,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: "Register failed, please try again");
+    }
+
+    var data = jsonDecode(response.body);
+
+    User.token = data['data']['token'];
+    User value = User.fromJson(data['data']['user']);
+
+    // Upload picture
+    if (pictureFile != null) {
+      ApiReturnValue<String> result = await uploadPicturePath(pictureFile);
+    }
+
+    return ApiReturnValue();
+  }
+
+  static Future<ApiReturnValue<String>> uploadPicturePath(
+      File picture, {http.MultipartRequest? request}) async {
+    String url = baseUrl + '/user/photo';
+
+    var uri = Uri.parse(url);
+
+    if (request == null) {
+      request = http.MultipartRequest('POST', uri)
+        ..headers['Content-Type'] = 'application/json'
+        ..headers['Authorization'] = 'Bearer ${User.token}';
+    }
+
+    var multipartFile = await http.MultipartFile.fromPath('file', picture.path);
+
+    request.files.add(multipartFile);
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      String responseBody = await response.stream.bytesToString();
+
+      var data = jsonDecode(responseBody);
+
+      String picturePath = data['data'][0];
+
+      return ApiReturnValue(value: picturePath);
+    } else {
+      return ApiReturnValue(message: "Failed To Upload Picture");
+    }
+  }
 }
