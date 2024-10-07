4273bd66288c586fdc38ddd73e45e2fcf1314e5b
AqilFarros <aqilghonim@gmail.com>
2024-10-07 11:31:51 +0700

    update

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,8 +1,28 @@
 part of 'services.dart';
 
 class TransactionService {
-  static Future<ApiReturnValue<List<Transaction>>> getTransactions() async {
-    await Future.delayed(const Duration(seconds: 3));
+  static Future<ApiReturnValue<List<Transaction>>> getTransactions(
+      {http.Client? client}) async {
+    // await Future.delayed(const Duration(seconds: 3));
+
+    // return ApiReturnValue(value: mockTransactions);
+
+    client ??= http.Client();
+
+    String url = '${baseUrl}transaction';
+
+    var response = await client.get(Uri.parse(url), headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${User.token}',
+    });
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Failed To Get Transactions');
+    }
+
+    var data = jsonDecode(response.body);
+
+    List<Transaction> value = (data['data']['data'] as Iterable).map((e) => Transaction.fromJson(e)).toList();
 
     return ApiReturnValue(value: mockTransactions);
   }
@@ -11,7 +31,8 @@ class TransactionService {
       Transaction transaction) async {
     await Future.delayed(const Duration(seconds: 2));
 
-    return ApiReturnValue(value: transaction.copyWith(
+    return ApiReturnValue(
+        value: transaction.copyWith(
       id: 123,
       status: TransactionStatus.pending,
       total: (transaction.quantity! * transaction.food!.price!).toInt(),
